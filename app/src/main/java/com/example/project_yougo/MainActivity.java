f7e6ec2f370fc7d7d488593d4e4335654fb8d260
Or Kravitz <63398613+imorkravitz@users.noreply.github.com>
2022-02-28 19:24:58 +0200

    Update MainActivity.java
    fix bug

--- app/src/main/java/com/example/project_yougo/MainActivity.java
+++ app/src/main/java/com/example/project_yougo/MainActivity.java
@@ -1,24 +1,12 @@
 package com.example.project_yougo;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.NavHost;
-import androidx.viewpager.widget.ViewPager;
-
 import android.os.Bundle;
 
-import com.google.android.material.tabs.TabLayout;
-
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
-
-    private ViewPagerAdapter viewPagerAdapter;
-    private TabLayout tabLayout;
     NavController navController;
-    ViewPager viewPager;
-    ArrayList<Fragment> fragments = new ArrayList<Fragment>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         NavHost navHost = (NavHost) getSupportFragmentManager().findFragmentById(R.id.base_navHost);
-        navController = navHost.getNavController();
+        navController = navHost != null ? navHost.getNavController() : null;
 
-        viewPager = findViewById(R.id.viewpager);
-
-        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
-
-        viewPagerAdapter.add(new RegisterFragment(), "Login");
-        viewPagerAdapter.add(new LoginFragment(), "Register");
-
-        viewPager.setAdapter(viewPagerAdapter);
-        tabLayout =  findViewById(R.id.tab_layout);
-        tabLayout.setupWithViewPager(viewPager);
     }
 }
