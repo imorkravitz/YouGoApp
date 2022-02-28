b8383cad9c194e9e442cea6fb3ce3abbe45ad12c
Or Kravitz <63398613+imorkravitz@users.noreply.github.com>
2022-02-28 19:24:34 +0200

    Update LoginFragment.java
    fix bug

--- app/src/main/java/com/example/project_yougo/LoginFragment.java
+++ app/src/main/java/com/example/project_yougo/LoginFragment.java
@@ -1,23 +1,19 @@
 package com.example.project_yougo;
 
 import android.os.Bundle;
-
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
-
+import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 
 public class LoginFragment extends Fragment {
 
     EditText email, password;
     Button login;
-    ImageView btn;
 
     public LoginFragment(){
         //required empty public constructor.
@@ -32,16 +28,17 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_register,container, false);
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_login, container, false);
 
         this.email = view.findViewById(R.id.login_email);
         this.password = view.findViewById(R.id.login_password);
-        this.login = view.findViewById(R.id.login_login_btn);
-        this.btn = view.findViewById(R.id.back_to_register);
+        this.login = (Button) view.findViewById(R.id.login_login_btn);
 
+        login.setOnClickListener((v)->{
+            Navigation.findNavController(v).navigate(R.id.action_homeFragment_to_postListFragment);
+        });
 
 
         return view;
     }
-
 }
