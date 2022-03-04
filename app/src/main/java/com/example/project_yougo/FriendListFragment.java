1317c8cf4e2415f71534deea4fe9a5cf930617b8
biskall <itaibiskall@gmail.com>
2022-03-04 22:55:09 +0200

    Main menu update

--- app/src/main/java/com/example/project_yougo/FriendListFragment.java
+++ app/src/main/java/com/example/project_yougo/FriendListFragment.java
@@ -24,107 +24,6 @@ import java.util.List;
 
 
 public class FriendListFragment extends Fragment {
-<<<<<<< Updated upstream
-    MyAdapterListFriends adapterListFriends;
-    List<Friend> data;
-        @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view =inflater.inflate(R.layout.fragment_friend_list, container, false);
-        data = Model.instance.getAllFriends();
-        RecyclerView list = view.findViewById(R.id.friendslist_rv);
-
-
-        list.setLayoutManager(new LinearLayoutManager(getContext()));
-
-        list.setHasFixedSize(true);
-        adapterListFriends = new MyAdapterListFriends();
-
-        list.setAdapter(adapterListFriends);
-        adapterListFriends.setOnItemClickListener(new OnItemClickListener() {
-                @Override
-                public void onItemClick(int position) {
-                    Log.d("Tag", "click on position" + position);
-                }
-        });
-        return view;
-    }
-
-    class MyViewHolderListFriends extends RecyclerView.ViewHolder{
-        TextView nameTv;
-        Button followBtn;
-        ImageView avatarImg;
-
-        public MyViewHolderListFriends(@NonNull View itemView, OnItemClickListener listener) {
-            super(itemView);
-            nameTv = itemView.findViewById(R.id.friendsList_name_tv);
-            followBtn = itemView.findViewById(R.id.friendsList_follow_btn);
-            avatarImg = itemView.findViewById(R.id.friendsList_avatar_img);
-            followBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int pos = getAdapterPosition();
-                    if(followBtn.getText() == "Follow") {
-                        followBtn.setText("Unfollow");
-                        data.get(pos).setFollow(true);
-                    }
-                    else {
-                        followBtn.setText("Follow");
-                        data.get(pos).setFollow(false);
-                    }
-                }
-            });
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int pos = getAdapterPosition();
-                    listener.onItemClick(pos);
-                }
-            });
-        }
-
-
-    }
-
-    interface OnItemClickListener{
-        void onItemClick(int position);
-    }
-
-    class MyAdapterListFriends extends RecyclerView.Adapter<MyViewHolderListFriends>{
-
-        OnItemClickListener listener;
-        public void setOnItemClickListener(OnItemClickListener listener){
-            this.listener = listener;
-        }
-
-        @NonNull
-        @Override
-        public MyViewHolderListFriends onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = getLayoutInflater().inflate(R.layout.find_friends_row,parent,false);
-            MyViewHolderListFriends holder = new MyViewHolderListFriends(view,listener);
-            return holder;
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull MyViewHolderListFriends holder, int position) {
-            Friend friend = data.get(position);
-            holder.nameTv.setText(friend.getName());
-            if(friend.isFollow()){
-                holder.followBtn.setText("Unfollow");
-
-            }
-            else
-                holder.followBtn.setText("Follow");
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return data.size();
-        }
-    }
-=======
 //    MyAdapterListFriends adapterListFriends;
 //    //List<Friend> data;
 //        @Override
@@ -230,6 +129,4 @@ public class FriendListFragment extends Fragment {
 //    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 //        return super.onOptionsItemSelected(item);
 //    }
-
->>>>>>> Stashed changes
 }
