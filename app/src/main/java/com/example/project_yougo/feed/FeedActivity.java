7090cd8c338e2945a77e706963136185c9c400ae
Voiko <nadavv10@walla.com>
2022-03-23 15:55:28 +0200

    LogOut Function

--- app/src/main/java/com/example/project_yougo/feed/FeedActivity.java
+++ app/src/main/java/com/example/project_yougo/feed/FeedActivity.java
@@ -6,11 +6,16 @@ import androidx.navigation.NavController;
 import androidx.navigation.NavHost;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.project_yougo.R;
+import com.example.project_yougo.login.LoginActivity;
+import com.example.project_yougo.model.user.UserModel;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class FeedActivity extends AppCompatActivity {
 
     private NavHost navHost;
@@ -53,10 +58,20 @@ public class FeedActivity extends AppCompatActivity {
                 case R.id.aboutFragment_menu:
                     navController.navigate(R.id.action_global_aboutFragment);
                     break;
+                case R.id.logout_menu:
+                    UserModel.getInstance().logOut();
+                    toLoginActivity();
+                    break;
             }
         }else{
             return true;
         }
         return true;
     }
+    private void toLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
